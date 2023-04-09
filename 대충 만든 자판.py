7d938191dd94036a7ce29b8f308e13088b0114e0
박세현 <sarah1918@naver.com>
2023-04-09 13:45:34 +0900

    .

--- 대충 만든 자판.py
+++ 대충 만든 자판.py
@@ -1,20 +1,26 @@
 def solution(keymap, targets):
     answer = []
     for i in range(len(targets)):
+        Flag = True
         sum=0
-        flag=False
         for j in range(len(targets[i])):
+            if Flag==False:
+                continue
+            flag = False
             idx=100
             for k in range(len(keymap)):
                 if targets[i][j] in keymap[k]:
                     if keymap[k].index(targets[i][j])<idx:
                         idx=keymap[k].index(targets[i][j])
                         flag=True
+
             if flag==True:
                 sum+=(idx+1)
             else:
+                Flag=False
                 sum=-1
+                continue
         answer.append(sum)
     return answer
 
-print(solution(["AA"],["B"]))
+print(solution(["ABCE"] , ["ABDE"]))
