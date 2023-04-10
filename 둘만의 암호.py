97af565c60c403605ba01d0a4234d9f63b0ae3cf
박세현 <sarah1918@naver.com>
2023-04-10 21:44:01 +0900

    .

--- 둘만의 암호.py
+++ 둘만의 암호.py
@@ -11,3 +11,27 @@ s="aukks"
 skip="wbqd"
 index=5
 print(solution(s, skip, index))
+
+'''
+from string import ascii_lowercase
+
+def solution(s, skip, index):
+    result = ''
+
+    a_to_z = set(ascii_lowercase)
+    a_to_z -= set(skip)
+    a_to_z = sorted(a_to_z)
+    l = len(a_to_z)
+
+    dic_alpha = {alpha:idx for idx, alpha in enumerate(a_to_z)}
+
+    for i in s:
+        result += a_to_z[(dic_alpha[i] + index) % l]
+
+    return result
+
+s="aukks"
+skip="wbqd"
+index=5
+print(solution(s, skip, index))
+'''
