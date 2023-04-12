7196503f572c2b531aa75e92c848557d1ef2d109
박세현 <sarah1918@naver.com>
2023-04-12 11:39:41 +0900

    .

--- 크기가 작은 부분문자열.py
+++ 크기가 작은 부분문자열.py
@@ -2,18 +2,8 @@ def solution(t, p):
     answer = 0
     n=len(p)
     for i in range(len(t)-n+1):
-<<<<<<< HEAD
         sliced_lst=t[i:i+n]
         if int(sliced_lst)<=int(p):
-=======
-        sliced_lst=t[i:i+3]
-        s=""
-        for j in range(n):
-            s+=str(sliced_lst[j])
-            int(s)
-        print(s)
-        if s<=p:
->>>>>>> origin/main
             answer+=1
     return answer
 
