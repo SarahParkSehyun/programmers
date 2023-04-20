3cf072ac3c7b0a591700de1e9d54c323db2ee9e3
박세현 <sarah1918@naver.com>
2023-04-21 01:02:06 +0900

    .

--- Lv.1/푸드 파이트 대회.py
+++ Lv.1/푸드 파이트 대회.py
@@ -1,3 +1,12 @@
 def solution(food):
-    answer = ''
-    return answer
+    answer = []
+    for i in range(1,len(food)):
+        num=food[i]//2
+        for _ in range(num):
+            answer.append(i)
+    reversed_answer=answer[::-1]
+    answer.append(0)
+    answer+=reversed_answer
+    answer=[str(x) for x in answer]
+    return ''.join(answer)
+print(solution([1, 3, 4, 6]))
