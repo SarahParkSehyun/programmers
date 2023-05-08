583b39e1a400d2dc34b988012ae231b8c625fb02
박세현 <sarah1918@naver.com>
2023-05-09 00:10:27 +0900

    .

--- Lv.1/[1차] 다트 게임.py
+++ Lv.1/[1차] 다트 게임.py
@@ -1,32 +1,30 @@
 def solution(dartResult):
-    answer = 0
+    answer = []
     for _ in range(3):
         digit=""
         idx=0
-        save=0
         for i in range(2):
             if dartResult[i].isdigit():
                 digit+=dartResult[i]
                 idx+=1
         if dartResult[idx]=='S':
-            save=int(digit)
-            answer+=int(digit)
+            answer.append(int(digit))
         elif dartResult[idx]=='D':
-            save = int(digit)
-            answer+=int(digit)**2
+            answer.append(int(digit)**2)
         else:
-            save = int(digit)
-            answer+=int(digit)**3
+            answer.append(int(digit)**3)
         idx+=1
-        print(idx)
-        if idx!=len(dartResult)-1 and dartResult[idx]=='*':
+        if idx!=len(dartResult) and dartResult[idx]=='*':
             idx+=1
-            answer=answer*2
-        if idx!=len(dartResult)-1 and dartResult[idx]=='#':
+            if len(answer)>1:
+                answer[-2]=answer[-2]*2
+                answer[-1]=answer[-1]*2
+            else:
+                answer[-1]=answer[-1]*2
+        if idx!=len(dartResult) and dartResult[idx]=='#':
             idx+=1
-            answer-=save*2
+            answer[-1]=answer[-1]*(-1)
         dartResult=dartResult[idx:]
+    return sum(answer)
 
-    return answer
-
-print(solution('1S2D*3T'))
+print(solution("1S2D*3T"))
